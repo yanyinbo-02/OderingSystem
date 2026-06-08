9ff996c87b6ea1287e618d2fea3c60af724123e4
yanyinbo <1563247416@qq.com>
2026-06-08 20:51:14 +0800

    完善 MemberManager 类，增加头文件保护及成员函数声明

--- src/models/member.h
+++ src/models/member.h
@@ -1,1 +1,25 @@
-//会员模型
+// 会员模型
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include <QString>
+
+class MemberManager {
+public:
+    static MemberManager& instance();
+
+    // 获取会员专属折扣系数
+    double getDiscount(const QString &memberId);
+    
+    // 累加会员积分
+    void addPoints(const QString &memberId, double finalPrice);
+
+    // 获取当前积分
+    int getPoints() const;
+
+private:
+    MemberManager();
+    int m_points; // 维护单用户的动态积分
+};
+
+#endif // MEMBER_H
