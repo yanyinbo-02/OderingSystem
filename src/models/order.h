a38f9deb8be6c761bef5dcc578d4acb4c420538b
yanyinbo <1563247416@qq.com>
2026-06-08 20:51:38 +0800

    完善 OrderManager 类，增加头文件保护及成员函数声明

--- src/models/order.h
+++ src/models/order.h
@@ -1,1 +1,29 @@
 //订单与历史记录模型
+// 订单管理模型
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <QString>
+#include <QList>
+#include "../controllers/system_engine.h"
+
+class OrderManager {
+public:
+    static OrderManager& instance();
+
+    // 增加新订单
+    void addOrder(const QString &dishName);
+    
+    // 获取历史订单列表
+    QList<OrderModel> getOrders();
+    
+    // 从界面提取的文本中反向克隆创建订单
+    bool duplicateFromText(const QString &itemText);
+
+private:
+    OrderManager();
+    QList<OrderModel> m_orders;
+    int m_orderIdCounter;
+};
+
+#endif // ORDER_H
