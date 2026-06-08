9059444690c1b4684da19e925a95cf5d47fb1330
yanyinbo <1563247416@qq.com>
2026-06-08 20:50:45 +0800

    完善 DishManager 类，增加头文件保护和静态桥接函数

--- src/models/dish.h
+++ src/models/dish.h
@@ -1,1 +1,27 @@
-//菜品与菜单模型
+/// 菜品与菜单模型
+#ifndef DISH_H
+#define DISH_H
+
+#include <QString>
+#include <QList>
+#include "../controllers/system_engine.h"
+
+class DishManager {
+public:
+    static DishManager& instance();
+    
+    // 【关键适配修正】提供静态桥接函数，直接适配 system_engine.cpp 内部的底层调用
+    static QList<DishModel> getFixedDishes() {
+        return instance().getDishes();
+    }
+    
+    QList<DishModel> getDishes();
+    double getDishPrice(const QString &name);
+    QList<CommentModel> getSortedComments(int index);
+
+private:
+    DishManager();
+    QList<DishModel> m_dishes;
+};
+
+#endif // DISH_H
