a1216743bc61a579d1ad0d8a4c91a12d33953e57
yanyinbo <1563247416@qq.com>
2026-06-14 08:13:43 +0800

    更新评论内容，简化描述

--- src/controllers/system_engine.cpp
+++ src/controllers/system_engine.cpp
@@ -39,11 +39,11 @@ QList<CommentModel> SystemEngine::getSortedComments(int index) {
     // 固定已有的评价数据
     QList<CommentModel> comments;
     QDateTime now = QDateTime::currentDateTime();
-    comments.append(CommentModel{5, "味道超级棒，招牌红烧肉肥而不腻！", now.addDays(-2)});
-    comments.append(CommentModel{4, "上菜速度挺快的，服务态度很好。", now.addDays(-1)});
-    comments.append(CommentModel{5, "每次来必点宫保鸡丁，绝美！", now.addSecs(-5 * 3600)});
-    comments.append(CommentModel{3, "环境还可以，但是排队的人稍微有点多。", now.addSecs(-2 * 3600)});
-    comments.append(CommentModel{2, "鱼肉稍微有点腥，希望下次能改进下。", now.addSecs(-30 * 60)});
+    comments.append(CommentModel{5, "招牌红烧肉肥而不腻", now.addDays(-2)});
+    comments.append(CommentModel{4, "服务态度很好。", now.addDays(-1)});
+    comments.append(CommentModel{5, "每次来必点宫保鸡丁", now.addSecs(-5 * 3600)});
+    comments.append(CommentModel{3, "排队的人稍微有点多。", now.addSecs(-2 * 3600)});
+    comments.append(CommentModel{2, "鱼肉稍微有点腥", now.addSecs(-30 * 60)});
 
     // 排序逻辑：0-按评分从高到低，1-按留言时间先后
     if (index == 0) {
